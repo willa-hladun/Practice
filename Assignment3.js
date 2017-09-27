008204296eb46fab1aa9bb8235f4d1aab7743983
Willa Hladun <willahladun@Willas-MBP.hitronhub.home>
2017-09-26 22:33:08 -0700

    Added a file

---
+++ Assignment3.js
@@ -0,0 +1,6 @@
+<script language="JavaScript" type="text/javascript">
+    var string = prompt("Type some letters!");
+    var textArr=string.split(", ");
+    textArr.sort();
+    alert(textArr.join(', '));
+</script>
